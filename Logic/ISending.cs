c286b62db1ddc8333058c3982b385fde18d49df4
ZhekaMoroz <kosorotova.e@mail.ru>
2020-01-09 17:39:28 +0300

    correct ISending

--- Logic/ISending.cs
+++ Logic/ISending.cs
@@ -6,7 +6,6 @@ namespace Send
 {
     interface ISending
     {
-        static IConnection Connection { get; set; }
         static void CreateConnection() { }
         static int SendMessage(EndpointData endpoint) { return 501; }
     }
